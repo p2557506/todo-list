ed04dcdb2b9d452e3412191fec60ec9e327fa0c0
Isaac Zerououl <p2557506@my365.dmu.ac.uk>
2022-09-27 17:13:13 +0100

    Added username and password input and background

--- auth/index.php
+++ auth/index.php
@@ -11,17 +11,24 @@
                     <div class = "row">
                         <!-- username -->
                         <div class = "col-lg-12">
-                            
+                            <label for="username">
+                                Your Username
+                            </label>
+                            <input class = "form-control" id="username">
                         </div>
                         <!-- password -->
                         <div class = "col-lg-12">
+                        <label for= "password">
+                                Your Password
+                            </label>
+                            <input type="password" class = "form-control" id = "password">
 
                         </div>
                     </div>
                 </div>
-                <div class = "col-lg-8">
-                    <!-- image -->
-
+                <!-- image -->
+                <div class = "col-lg-8" style = "background-image: url('https://images.hdqwalls.com/download/beautiful-evening-landscape-minimal-8k-ag-2560x1440.jpg'); height: 100vh">
+                    
                 </div>
             </div>
 
