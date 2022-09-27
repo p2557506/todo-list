494739b7237079cc2c956c46959f5263411a4e20
malikbensalem <60427086+malikbensalem@users.noreply.github.com>
2022-09-27 18:24:43 +0100

    landing page

--- index.php
+++ index.php
@@ -4,8 +4,22 @@
         <?include $_SERVER['ROOT_PATH']."includes/head.php"?>
     </head>
     <body>
-        <div class = "container">
-            <select id=""></select>
+    
+        <div class="row" style="background-image: url('https://poloandtweed.com/wp-content/uploads/2019/07/Tips-to-Managing-Tasks.jpg'); padding:10vh;">
+            <div class="col-sm-12 text-center">
+                <img src="https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/20/e1/24/20e124a4-89d4-4bb0-5dc4-27036b805755/AppIcon-Release-0-0-85-220-0-0-0-0-4-0-0-0-2x-sRGB-0-0-0-0-0.png/1200x630bb.png" height="100" alt="logo">
+            </div>
+            <div class="col-sm-12 text-center">
+                <button class="btn btn-primary">Login or Register</button>
+            </div>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                
+            </div>
+        </div>
+        
         </div>
     </body>
 </html>
