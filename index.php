7b35219feae61583de8978e881a102ee71421db0
p2557506 <p2557506@my365.dmu.ac.uk>
2022-09-27 15:43:31 +0100

    Login page

--- index.php
+++ index.php
@@ -3,5 +3,39 @@
         <title>todo</title>
         <?include $_SERVER['ROOT_PATH']."includes/head.php"?>
     </head>
-    <body></body>
+    <body>
+        <h1>Login Page</h1>
+        <div class = "container">
+            <div class = "row">
+                <div class = "col-lg-1">
+                    Your Email
+                </div>
+
+                <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+  
+
+                </div>
+                
+                <div class = "col-lg-3">
+                    Your Password
+                </div>
+                <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Password" aria-label="Username">
+  
+
+                </div>
+
+                
+                <div class = "col-lg-1">
+                <button type="button" class="btn btn-outline-primary">Register</button>
+                </div>
+
+                <div class = "col-lg-1">
+                <button type="button" class="btn btn-outline-primary">Login</button>
+                </div>
+            </div>
+
+        </div>
+    </body>
 </html>
