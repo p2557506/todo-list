0548b3adcf988e679273a3031aadc20c65749293
Isaac Zerououl <p2557506@my365.dmu.ac.uk>
2022-09-29 14:41:08 +0100

    Update index.php
    -Adjusted background image
    -Added login and register button

--- auth/index.php
+++ auth/index.php
@@ -3,8 +3,8 @@
         <title>todo</title>
         <?include $_SERVER['ROOT_PATH']."includes/head.php"?>
     </head>
-    <body>
-        <h1>Login Page</h1>
+    <body style="margin: 0;">
+        <h1 style="margin: 0;">Login Page</h1>
         <div class = "container">
             <div class = "row">
                 <div class = "col-lg-4">
@@ -22,12 +22,25 @@
                                 Your Password
                             </label>
                             <input type="password" class = "form-control" id = "password">
-
                         </div>
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">Login</span>
+                            
+                            </button>
+                        </div>
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">Register</span>
+                            
+                            </button>
+                        </div>
+
                     </div>
                 </div>
                 <!-- image -->
-                <div class = "col-lg-8" style = "background-image: url('https://images.hdqwalls.com/download/beautiful-evening-landscape-minimal-8k-ag-2560x1440.jpg'); height: 100vh">
+                <div class = "col-lg-8" style = "background-image: url('https://images.hdqwalls.com/download/beautiful-evening-landscape-minimal-8k-ag-2560x1440.jpg'); 
+                height: 100vh; width: 100vw; background-size:cover; background-position: center;">
                     
                 </div>
             </div>
