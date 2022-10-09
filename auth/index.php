090321540f62158f12639c91145422cb133904a1
malikbensalem <60427086+malikbensalem@users.noreply.github.com>
2022-10-09 20:20:17 +0100

    -Centred form, background - img full screen, buttons styled

--- auth/index.php
+++ auth/index.php
@@ -3,48 +3,49 @@
         <title>todo</title>
         <?include $_SERVER['ROOT_PATH']."includes/head.php"?>
     </head>
-    <body style="margin: 0;">
-        <h1 style="margin: 0;">Login Page</h1>
-        <div class = "container">
-            <div class = "row">
-                <div class = "col-lg-4">
-                    <div class = "row">
-                        <!-- username -->
-                        <div class = "col-lg-12">
-                            <label for="username">
-                                Your Username
-                            </label>
-                            <input class = "form-control" id="username">
-                        </div>
-                        <!-- password -->
-                        <div class = "col-lg-12">
-                        <label for= "password">
-                                Your Password
-                            </label>
-                            <input type="password" class = "form-control" id = "password">
-                        </div>
-                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">Login</span>
-                            
-                            </button>
-                        </div>
-                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">Register</span>
-                            
-                            </button>
-                        </div>
-
+    <body>
+        <div class = "row" style="background-image: url('https://images.hdqwalls.com/download/beautiful-evening-landscape-minimal-8k-ag-2560x1440.jpg'); height:100vh;background-position: center;">
+            <div style="margin: auto;">
+                <div class = "row">
+                    <div class="offset-lg-4 col-lg-4">
+                        <h1 class = "text-center">Login Page</h1>
                     </div>
                 </div>
-                <!-- image -->
-                <div class = "col-lg-8" style = "background-image: url('https://images.hdqwalls.com/download/beautiful-evening-landscape-minimal-8k-ag-2560x1440.jpg'); 
-                height: 100vh; width: 100vw; background-size:cover; background-position: center;">
-                    
-                </div>
-            </div>
-
+                <div class = "row">
+                    <div class="offset-lg-4 col-lg-4" style="background-color: rgba(0,0,255,0.2);">
+                        <div class = "row">
+                            <!-- username -->
+                            <div class = "col-lg-12">
+                                <label for="username">
+                                    Your Username
+                                </label>
+                                <input class = "form-control" id="username">
+                            </div>
+                            <!-- password -->
+                            <div class = "col-lg-12">
+                                <label for= "password">
+                                    Your Password
+                                </label>
+                                <input type="password" class = "form-control" id = "password">
+                            </div>
+                            <div class = "col-lg-12">                                
+                                <button class="btn btn-primary btn-block mt-2" style="width: 100%;">
+                                    Login
+                                </button>
+                            </div>
+                            <div class = "col-lg-12">                                
+                                <button class="btn btn-primary btn-block mt-2 mb-2" style="width: 100%;">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>              
+            </div>              
         </div>
+
+        <script>
+            // on click show alert 
+        </script>
     </body>
 </html>
--- index.php
+++ index.php
@@ -9,20 +9,16 @@
     <body>
     
         <div class="row" style="background-image: url('https://poloandtweed.com/wp-content/uploads/2019/07/Tips-to-Managing-Tasks.jpg'); padding:10vh;">
-            <div class="col-sm-12 text-center">
+            <div class="offset-md-4 col-sm-4 text-center pb-2" style="background-color: rgba(255,255,255,.5);">
                 <img src="https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/20/e1/24/20e124a4-89d4-4bb0-5dc4-27036b805755/AppIcon-Release-0-0-85-220-0-0-0-0-4-0-0-0-2x-sRGB-0-0-0-0-0.png/1200x630bb.png" height="100" alt="logo">
-            </div>
-            <div class="col-sm-12 text-center">
                 <h1>Todo list</h1>
-                <a href="auth/" class="btn btn-primary">Login or Register</a>
+                <a href="auth/" class="btn btn-primary"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login or Register</a>
             </div>
         </div>
         <div class="container">
         <br>
         <div class="row">
-        <div class="col-md-3">
-        </div>
-            <div class="col-md-6">
+            <div class="offset-md-3 col-md-6">
                 <h2>About</h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita voluptates libero saepe facilis sed provident, consequatur quis non dolores, ullam, nemo in? Magnam quasi cum debitis ipsa repellendus laboriosam fugiat?
