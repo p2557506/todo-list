180e373706f41bd6c84e8d321c07fab6804aa44b
malikbensalem <60427086+malikbensalem@users.noreply.github.com>
2022-10-29 22:58:47 +0100

    Create nav.php

---
+++ includes/php/nav.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Todo-List</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="nav">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Link1</a>
+        <a class="nav-link" href="#">Link2</a>
+        <a class="nav-link disabled">Logout</a>
+      </div>
+    </div>
+  </div>
+</nav>
